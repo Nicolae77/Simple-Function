c790762f69a4d25b6187e4e40fdf45f0d5402c30
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-05-20 12:52:06 +0100

    Update main.py

--- simple function/main.py
+++ simple function/main.py
@@ -3,15 +3,15 @@ def add(n1, n2):
 
 
 def subtraction(n1, n2):
-    return n1 + n2
+    return n1 - n2
 
 
 def multiply(n1, n2):
-    return n1 + n2
+    return n1 * n2
 
 
 def divide(n1, n2):
-    return n1 + n2
+    return n1 / n2
 
 
 def calculator(n1, n2, func):
@@ -19,9 +19,9 @@ def calculator(n1, n2, func):
 
 
 add_result = calculator(2, 3, add)
-subtraction_result = calculator(5, 3, add)
-multiply_result = calculator(4, 3, add)
-divide_result = calculator(6, 3, add)
+subtraction_result = calculator(5, 3, subtraction)
+multiply_result = calculator(4, 3, multiply)
+divide_result = calculator(6, 3, divide)
 print(add_result)
 print(subtraction_result)
 print(multiply_result)
